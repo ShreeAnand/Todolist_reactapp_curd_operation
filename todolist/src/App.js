82c72aeb2a763fcf10cc0df9c5fa90e1969de1fb
Shreelaksmi Anand <shreeputta20@gmail.com>
2023-02-13 12:11:27 +0100

    curd operations changes

---
+++ todolist/src/App.js
@@ -0,0 +1,10 @@
+import Todo from "./components/todoList/todo";
+function App() {
+  return (
+<>
+<Todo />
+</>
+);
+}
+
+export default App;
---
+++ todolist/src/components/todoList/todo.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './style.css';
+
+
+const Todo = () => {
+  return (
+    <div className='main-div'>
+      <div className='child-div'> 
+      <figure>
+      <img src="./images/todo.svg" alt="todologo" />
+      <figcaption>Add Your List Here </figcaption>
+      </figure>
+      <div className='addItems'> 
+      <input type="text"  placeholder='Add Item' className='form-control'/>
+      <i className='fa fa-plus add-btn'></i>
+       </div>
+       <div className='showItems'>
+        <button className='btn effect04' data-sm-link-text="Remove All"><span>CHECK LIST</span></button>
+      </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Todo
